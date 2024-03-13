5a41e031613e3faf16ed5c1eee28f4f020e97228
ming-guang <athoni-contact@relay.akimitsu.dev>
2024-03-13 11:13:13 +0700

    feat(practice-1): implement range function for subtask 1

--- practice-1/source/main.py
+++ practice-1/source/main.py
@@ -1,15 +1,39 @@
 import numpy as np
 
 
+def nanptp(inp, axis=None):
+    if axis == 0:
+        return [nanptp(np.array(inp[:, i])) for i in range(inp.shape[1])]
+    if axis == 1:
+        return [nanptp(np.array(row)) for row in inp]
+    target = [[]]
+    if len(inp.shape) > 1:
+        target = [[x for x in row if not np.isnan(x)] for row in inp]
+    else:
+        target = [[x for x in inp if not np.isnan(x)]]
+    min = np.inf
+    max = -np.inf
+    for row in target:
+        for x in row:
+            if x < min:
+                min = x
+            if x > max:
+                max = x
+    return max - min
+
+
 # Mean, Median, Max, Min, Range
 def sub_task_1(n, size, inp):
     print("mean: ", np.nanmean(inp))
     print("median: ", np.nanmedian(inp))
+    print("range: ", nanptp(inp))
     if n > 1:
         print("mean by column: ", np.nanmean(inp, axis=0))
         print("mean by row: ", np.nanmean(inp, axis=1))
         print("median by column: ", np.nanmedian(inp, axis=0))
         print("median by row: ", np.nanmedian(inp, axis=1))
+        print("range by row: ", nanptp(inp, axis=0))
+        print("range by column: ", nanptp(inp, axis=1))
 
 
 # Variance & Standard Deviation
