cf66c155e1976caeea8fa2e7e6b43aa4125f4514
ming-guang <athoni-contact@relay.akimitsu.dev>
2024-03-13 10:11:11 +0700

    feat(practice-1): implement subtask 2

--- practice-1/source/main.py
+++ practice-1/source/main.py
@@ -3,17 +3,19 @@ import numpy as np
 
 # Mean, Median, Max, Min, Range
 def sub_task_1(n, size, inp):
-    print("mean: ", np.mean(inp))
-    print("median: ", np.median(inp))
+    print("mean: ", np.nanmean(inp))
+    print("median: ", np.nanmedian(inp))
     if n > 1:
-        print("mean by column: ", np.mean(inp, axis=0))
-        print("mean by row: ", np.mean(inp, axis=1))
-        print("median by column: ", np.median(inp, axis=0))
-        print("median by row: ", np.median(inp, axis=1))
+        print("mean by column: ", np.nanmean(inp, axis=0))
+        print("mean by row: ", np.nanmean(inp, axis=1))
+        print("median by column: ", np.nanmedian(inp, axis=0))
+        print("median by row: ", np.nanmedian(inp, axis=1))
 
 
 # Variance & Standard Deviation
 def sub_task_2(n, size, inp):
+    print("var: ", np.nanvar(inp))
+    print("std: ", np.nanstd(inp))
     pass
 
 
@@ -22,11 +24,18 @@ def sub_task_3(n, size, inp):
     pass
 
 
+def float_or_nan(n):
+    try:
+        return float(n)
+    except ValueError:
+        return np.NaN
+
+
 def main():
     n: int = int(input("number of samples: "))
     size: int = int(input("sample size: "))
     inp = [
-        [int(input("inp[{}][{}]: ".format(i, j))) for j in range(size)]
+        [float_or_nan(input("inp[{}][{}]: ".format(i, j))) for j in range(size)]
         for i in range(n)
     ]
     inp = np.array(inp)
